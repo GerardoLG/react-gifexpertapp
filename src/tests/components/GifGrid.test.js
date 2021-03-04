1d8d6f18477494be0bcabeec736e6d3e81cfc2d3
Gerardo Lopez <59430166+GerardoLG@users.noreply.github.com>
2021-03-03 20:29:15 -0600

    Tests on GifGrid component

---
+++ src/tests/components/GifGrid.test.js
@@ -0,0 +1,45 @@
+import { shallow } from "enzyme";
+import { GifGrid } from "../../components/GifGrid";
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+
+jest.mock("../../hooks/useFetchGifs");
+
+describe("Tests on <GifGrid /> component", () => {
+  const category = "Hola mundo";
+
+  test("should show <GifGrid /> correctly", () => {
+    useFetchGifs.mockReturnValue({
+      data: [],
+      loading: true,
+    });
+
+    const wrapper = shallow(<GifGrid category={category} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("should show items when images are loaded from useFetchGifs", () => {
+    const gifs = [
+      {
+        id: "ABC",
+        url: "https://localhost/cualquier/cosa.jpg",
+        title: "Cualquier cosa",
+      },
+      {
+        id: "DEF",
+        url: "https://localhost/cualquier/cosa.jpg",
+        title: "Cualquier cosa",
+      },
+    ];
+
+    useFetchGifs.mockReturnValue({
+      data: gifs,
+      loading: false,
+    });
+
+    const wrapper = shallow(<GifGrid category={category} />);
+
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find("p").exists()).toBe(false);
+    expect(wrapper.find("GifGridItem").length).toBe(gifs.length);
+  });
+});
