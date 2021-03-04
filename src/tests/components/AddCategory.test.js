4fa474323f273f35ecb4c2fbd7f41f6b63e8a363
Gerardo Lopez <59430166+GerardoLG@users.noreply.github.com>
2021-03-03 18:28:17 -0600

    Tests on AddCategory component

---
+++ src/tests/components/AddCategory.test.js
@@ -0,0 +1,41 @@
+import { shallow } from "enzyme";
+import { AddCategory } from "../../components/AddCategory";
+
+describe("Tests on <AddCategory />", () => {
+  const setCategories = jest.fn();
+  let wrapper = shallow(<AddCategory setCategories={setCategories} />);
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    wrapper = shallow(<AddCategory setCategories={setCategories} />);
+  });
+
+  test("should show <AddCategory /> correctly", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("should change the input", () => {
+    const input = wrapper.find("input");
+    const value = "Hola Mundo";
+
+    input.simulate("change", { target: { value } });
+    expect(wrapper.find("p").text().trim()).toBe(value);
+  });
+
+  test("should NOT post information on submit", () => {
+    wrapper.find("form").simulate("submit", { preventDefault() {} });
+    expect(setCategories).not.toHaveBeenCalled();
+  });
+
+  test("should call Categories and clean textbox", () => {
+    const input = wrapper.find("input");
+    const value = "Hola Mundo";
+    input.simulate("change", { target: { value } });
+
+    wrapper.find("form").simulate("submit", { preventDefault() {} });
+    expect(setCategories).toHaveBeenCalled();
+
+    input.simulate("change", { target: { value: "" } });
+    expect(wrapper.find("input").prop("value")).toBe("");
+  });
+});
